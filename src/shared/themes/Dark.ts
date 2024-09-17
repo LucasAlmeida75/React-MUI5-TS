23d6422a6c77838c855903ae7f9cf1769fc76dc1
LucasAlmeida75 <lucasalmeidamachado1@gmail.com>
2024-09-17 13:03:10 -0300

    feat: temas e organização de pastas

---
+++ src/shared/themes/Dark.ts
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+import { blue, green } from "@mui/material/colors";
+
+export const DarkTheme = createTheme({
+    palette: {
+        primary: {
+            main: blue[700],
+            dark: blue[800],
+            light: blue[500],
+            contrastText: "#ffffff"
+        },
+        secondary: {
+            main: green[500],
+            dark: green[400],
+            light: green[300],
+            contrastText: "#ffffff"
+        },
+        background: {
+            default: '#303134',
+            paper: '#202124'
+        }
+    }
+})
---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+import { blue, green } from "@mui/material/colors";
+
+export const LightTheme = createTheme({
+    palette: {
+        primary: {
+            main: blue[700],
+            dark: blue[800],
+            light: blue[500],
+            contrastText: "#ffffff"
+        },
+        secondary: {
+            main: green[500],
+            dark: green[400],
+            light: green[300],
+            contrastText: "#ffffff"
+        },
+        background: {
+            default: '#ffffff',
+            paper: '#f7f6f3'
+        }
+    }
+})
